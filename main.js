5903b0ac4d868d280550c35e3e12d1f47dd14a51
Adhambek0202 <adhambekismatov0212@gmail.com>
2022-07-05 00:10:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var userName = prompt("Iltimos ismingizni kiriting..!")
+var familya = prompt("Iltimos familyangizni kiriting..!")
+var yosh = prompt("Yoshingizni kiriting..!")
+var telefon = prompt("Telefon raqamingizni kiriting..!")
+var manzil = prompt("Yashash manzilingiz..!")
+var unversitet = prompt("O'qish joyingiz..!")
+var web = prompt("O'qiydigan yo'nalishingiz..!")
+var sport = prompt("Yoqtirgan sport turingiz..!")
+var kompyuter = prompt("Kompyuteringiz modeli..!")
+var telefon = prompt("Telefoningiz modeli..!")
+
+
+alert(`Ismingiz; ${userName}\nFamilyagiz: ${familya}\nYoshingiz: ${yosh}\nTelefon raqamingiz: ${telefon}\nYashash manzilingiz: ${manzil}\nO'qish joyingiz: ${unversitet}\nYo'nalishingiz; ${web}\nYoqtirgan sport turingiz: ${sport}\nKompyuteringiz modeli: ${kompyuter}\nTelefoningiz modeli: ${telefon}`)
+console.log(`Ismingiz; ${userName}\nFamilyagiz: ${familya}\nYoshingiz: ${yosh}\nTelefon raqamingiz: ${telefon}\nYashash manzilingiz: ${manzil}\nO'qish joyingiz: ${unversitet}\nYo'nalishingiz; ${web}\nYoqtirgan sport turingiz: ${sport}\nKompyuteringiz modeli: ${kompyuter}\nTelefoningiz modeli: ${telefon}`)
